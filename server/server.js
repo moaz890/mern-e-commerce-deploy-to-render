6ea0f8b3c2927603c1fcf7dc68311e2b8addc1cf
moaz890 <mozagazzer@gmail.com>
2024-10-22 07:21:36 +0200

    reset middleware for cors

--- server/server.js
+++ server/server.js
@@ -24,12 +24,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(morgan('dev'));
-app.use(cors({
-    origin: "*",
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'X-Requested-With', 'Expires', 'Pragma'],
-    credentials: true
-}));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', process.env.CLIENT_ORIGIN);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cache-Control, X-Requested-With, Expires, Pragma');
+    res.header('Access-Control-Allow-Credentials', true);
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+});
 
 app.use(cookieParser());
 app.use(express.json());
